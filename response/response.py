b97cfdc1751a4332905fc459fc7a75b174c1fc5a
Cheng Ma <md11chm@gmail.com>
2022-03-25 00:31:21 -0400

    updated instructions

--- response/response.py
+++ response/response.py
@@ -1,3 +1,16 @@
+"""
+usage: 
+in your file, import response
+call response.respond("your word")
+will display a new custom page
+
+dependencies:
+pip install beautifulsoup4
+pip install Google-Images-Search
+pip install Wikipedia-API
+pip install Jinja2
+"""
+
 import sys
 import requests
 from bs4 import BeautifulSoup
@@ -8,17 +21,14 @@ from PIL import Image
 import get_photo
 import render_page
 
-"""
-dependencies:
-pip install beautifulsoup4
-pip install Google-Images-Search
-pip install Wikipedia-API
-pip install Jinja2
-"""
-
-if __name__ == "__main__":
-    #term must be in glossary of climate change terms
-    query = ' '.join(sys.argv[1:len(sys.argv)])
-    print(query)
+def respond(query):
     photo_addr = get_photo.display_photo(query)
     render_page.render(photo_addr, query)
+
+#for testing purposes
+# if __name__ == "__main__":
+#     #term must be in glossary of climate change terms
+#     query = ' '.join(sys.argv[1:len(sys.argv)])
+#     print(query)
+#     photo_addr = get_photo.display_photo(query)
+#     render_page.render(photo_addr, query)
