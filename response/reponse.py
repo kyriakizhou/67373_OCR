aa7e13251d77d304b6c291a5307224691f2761cf
Cheng Ma <md11chm@gmail.com>
2022-03-24 23:18:15 -0400

    displays photo locally

---
+++ response/reponse.py
@@ -0,0 +1,36 @@
+import sys
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from google_images_search import GoogleImagesSearch
+import urllib.request
+from PIL import Image
+
+#key for google image api
+gis = GoogleImagesSearch('AIzaSyCg1SymAm6-P-FM0dIEMpvXOvt-eNd9bH0', 'c0090d82ebd453980')
+
+_search_params = {
+    'q': '',
+    'num': 1,
+    'fileType': '',
+    'rights': '',
+    'safe': '', 
+    'imgType': '', 
+    'imgSize': '', 
+    'imgDominantColor': '',
+    'imgColorType': ''
+}
+
+def display_photo(query):
+    _search_params['q'] = query + ' climate'
+    gis.search(search_params=_search_params)
+    for image in gis.results():
+        print(f'image url: {image.url}')  # image direct url
+        image.download('./downloaded_images')  # download image
+        image.resize(500, 500)  # resize downloaded image
+        img = Image.open(image.path)
+        img.show()
+
+if __name__ == "__main__":
+    query = sys.argv[1]
+    display_photo(query)
