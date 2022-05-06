67af9196c7f587b7b8666593638648d29751df92
ccma1 <ccma@andrew.cmu.edu>
2022-05-06 12:26:48 -0400

    changes

--- response/render_page.py
+++ response/render_page.py
@@ -1,69 +1,20 @@
 import sys
-import requests
-from bs4 import BeautifulSoup
-from google_images_search import GoogleImagesSearch
-import urllib.request
-from PIL import Image
-import wikipediaapi
-
 import webbrowser
 
 from jinja2 import Environment, FileSystemLoader
 import os
 
-def get_wiki_info(query):
-    wiki_wiki = wikipediaapi.Wikipedia('en')
-    page_py = wiki_wiki.page(query)
-    page_summary = page_py.summary
-    print(f'Retrieved wikipedia summary')
-    return page_summary
-
-def load_page(text, query, path_to_photo):
-    # print(path_to_photo)
-    print('Creating page')
-    root = os.path.dirname(os.path.abspath(__file__))
-    templates_dir = os.path.join(root, 'templates')
-    env = Environment( loader = FileSystemLoader(templates_dir) )
-    template = env.get_template('dynamic.html')
-    
-    filename = os.path.join(root, 'html', 'index.html')
-    with open(filename, 'w', encoding="utf-8") as fh:
-        fh.write(template.render(
-            title = query,
-            h1 = query.capitalize(),
-            photo = f'<img src={"../."+str(path_to_photo)} alt="test">',
-            body = text
-        ))
-
-    chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
-
-    webbrowser.get(chrome_path).open_new_tab(filename)
-    # webbrowser.open_new_tab(filename)
-
-def load_demo_page(text, query, path_to_photo,video_link, isIndex):
-    # print(path_to_photo)
+def load_page(items):
     print('Creating page')
     root = os.path.dirname(os.path.abspath(__file__))
     templates_dir = os.path.join(root, 'templates')
     env = Environment( loader = FileSystemLoader(templates_dir) )
     template = env.get_template('demo.html')
     
-    fname = "index.html" if isIndex else (query+'index.html')
+    fname = "index.html"
     filename = os.path.join(root, 'html', fname)
     with open(filename, 'w', encoding="utf-8") as fh:
-        fh.write(template.render(
-            title = query,
-            h1 = query.capitalize(),
-            # photo = f'<img style="width:500px;" src={"../."+str(path_to_photo)} alt="test">',
-            photo = f'<img style="width:500px;" src={str(path_to_photo)} alt="test">',
-            video = video_link,
-            body = text
-        ))
-    chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
-
-    webbrowser.get(chrome_path).open_new_tab(filename)
-    # webbrowser.open_new_tab(filename)
-
-def render(path_to_photo, query):
-    wiki_content = get_wiki_info(query)
-    load_page(wiki_content, query, path_to_photo)
+        fh.write(template.render(items = items))
+    # chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+    # webbrowser.get(chrome_path).open_new_tab(filename)
+    webbrowser.open_new_tab(filename)
--- response/responding.py
+++ response/responding.py
@@ -1,58 +1,42 @@
 """
 usage: 
-in your file, import response
-call response.respond("your word")
-will display a new custom page
 
 dependencies:
-pip install beautifulsoup4
-pip install Google-Images-Search
-pip install Wikipedia-API
 pip install Jinja2
 """
 
 import sys
-import requests
-from bs4 import BeautifulSoup
-from google_images_search import GoogleImagesSearch
-import urllib.request
-from PIL import Image
-import random
 import pandas as pd
-
-from response import get_photo
-from response import render_page
 # import get_photo
-# import render_page
+import render_page
 
-demo = True
-
-def respond(query, isIndex):
+def respond(query):
     """
     if demo is true, query should be a set of terms from preprocess.py
     query string MUST be in demo_terms.csv
     """
-    if demo:
-        demo_data = pd.read_csv("response/demo_terms.csv")
-        path_to_photo = ""+query+".jpg"
-        # NOTE: debug
-        text = demo_data[demo_data["word"] == query]["description"].iloc[0]
-        video_link = demo_data[demo_data["word"]==query]["video_frame"].iloc[0]
-        render_page.load_demo_page(text,query,path_to_photo,video_link, isIndex)
-    else:
-        photo_addr = get_photo.display_photo(query)
-        print(photo_addr)
-        render_page.render(photo_addr, query)
+    # demo_data = pd.read_csv("response/demo_terms.csv")
+    demo_data = pd.read_csv("demo_terms.csv")
+    # path_to_photo = query+".jpg"
+    # # NOTE: debug
+    # text = demo_data[demo_data["word"] == query]["description"].iloc[0]
+    # video_link = demo_data[demo_data["word"]==query]["video_frame"].iloc[0]
+    # render_page.load_demo_page(text,query,path_to_photo,video_link, isIndex)
+    terms_list = []
+    for recognized_word in query:
+        item = dict(
+            term = recognized_word,
+            # image = f'<img src={"../."+recognized_word+".jpg"} alt="image for word">',
+            image = f'<img src={"../."+recognized_word+".jpg"} alt="image for word">',
+            video_link = demo_data[demo_data["word"]==recognized_word]["video_frame"].iloc[0],
+            text = demo_data[demo_data["word"] == recognized_word]["description"].iloc[0]
+        )
+        terms_list.append(item)
+    render_page.load_page(terms_list)
 
 #for testing purposes
 if __name__ == "__main__":
     #term must be in glossary of climate change terms
-    query = ' '.join(sys.argv[1:len(sys.argv)])
-    if demo:
-        respond(query)
-    else:
-        print(f'Your Query: {query}')
-        photo_addr = get_photo.display_photo(query)
-        print(f'Downloaded photo of {query}')
-        print(photo_addr)
-        render_page.render(photo_addr, query)
+    # query = ' '.join(sys.argv[1:len(sys.argv)])
+    query = ["carbondiet","bondevent"]
+    respond(query)
